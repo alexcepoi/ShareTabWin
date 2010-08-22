c44ddfbda6635f82f9f01d04b7ddfd5b71a5aae8
vene <vlad@vene.ro>
2010-08-22 14:50:02 +0300

    a couple of files left out of last commit

---
+++ Communication/IConnectParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Communication
+{
+    public interface IConnectParams
+    {
+        string Hostname { get; set; }
+        string Nickname { get; set; }
+        string Passkey { get; set; }
+        int Port { get; set; }
+    }
+}
---
+++ ShareTabWin/LogPanel.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ShareTabWin
+{
+	/// <summary>
+	/// Interaction logic for Tabs.xaml
+	/// </summary>
+	public partial class LogPanel : AvalonDock.DockableContent
+	{
+        public MyTraceListener myTraceListener { get; set; }
+		public LogPanel()
+		{
+			myTraceListener = new MyTraceListener ();
+			System.Diagnostics.Trace.Listeners.Add (myTraceListener);
+			DataContext = myTraceListener;
+			InitializeComponent();
+		}
+	}
+}
