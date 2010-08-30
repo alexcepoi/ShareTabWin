e2020253c8a27873d08a2112f9733f2bd754acf8
vene <vlad@vene.ro>
2010-08-30 13:35:45 +0300

    Fixed interop keypresses

---
+++ ShareTabWin/Helpers/Browser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShareTabWin.Helpers
+{
+	class Browser : Skybound.Gecko.GeckoWebBrowser
+	{
+		protected override bool IsInputKey (Keys keyData)
+		{
+			/*if ((keyData & Keys.Modifiers) == Keys.Control && (keyData & Keys.KeyCode) == Keys.O)
+			{
+				System.Diagnostics.Trace.TraceInformation ("ctrl o");
+				return false;
+			}
+			else
+			{
+				return base.IsInputKey (keyData);
+			}*/
+			return false;
+		}
+	}
+}
