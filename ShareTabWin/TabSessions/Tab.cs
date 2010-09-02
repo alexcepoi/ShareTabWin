9eda51e88651191b1c7b8db9fac2848f01f5f6f5
Alexandru Cepoi <alex.cepoi@gmail.com>
2010-09-02 04:11:36 +0300

    partial work at private session
    just for sync.. nothing works at present..

---
+++ ShareTabWin/TabSessions/Tab.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShareTabWin
+{
+	public class Tab : BrowserWindow
+	{
+		public Tab() : base()
+		{}
+	}
+}
---
+++ ShareTabWin/TabSessions/TabSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Collections.ObjectModel;
+
+namespace ShareTabWin
+{
+	public class TabSession
+	{
+		public static string DisplayName = "Private Tabs";
+
+		private ObservableCollection<Tab> m_Tabs;
+		public ObservableCollection<Tab> Tabs
+		{
+			get
+			{
+				if (m_Tabs == null)
+					m_Tabs = new ObservableCollection<Tab>();
+				return m_Tabs;
+			}
+		}
+
+		public TabSession()
+		{
+			Tabs.Add(new Tab());
+			Tabs.Add(new Tab());
+			Tabs.Add(new Tab());
+		}
+	}
+}
