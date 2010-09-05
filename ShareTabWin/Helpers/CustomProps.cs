ddc330bbdb893f00bb6ddb4a93fd1b85ff26bab5
vene <vlad@vene.ro>
2010-09-05 17:37:10 +0300

    Toolbar button styles

---
+++ ShareTabWin/Helpers/CustomProps.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ShareTabWin.Helpers
+{
+	public static class CustomProps
+	{
+		public static readonly DependencyProperty MyCornerRadiusProperty = DependencyProperty.RegisterAttached (
+			"MyCornerRadius", typeof (CornerRadius), typeof (CustomProps), new UIPropertyMetadata (new CornerRadius (2)));
+
+		public static CornerRadius GetMyCornerRadius (DependencyObject target)
+		{
+			return (CornerRadius) target.GetValue (MyCornerRadiusProperty);
+		}
+
+		public static void SetMyCornerRadius (DependencyObject target, CornerRadius value)
+		{
+			target.SetValue (MyCornerRadiusProperty, value);
+		}
+	}
+}
