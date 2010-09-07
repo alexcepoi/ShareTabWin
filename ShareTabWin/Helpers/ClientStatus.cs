2dba125082bf243218a048d3245249bc5e60893c
vene <vlad@vene.ro>
2010-09-07 14:35:08 +0300

    fixme alex

---
+++ ShareTabWin/Helpers/ClientStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShareTabWin
+{
+	public class ClientStatus : INotifyPropertyChanged
+	{
+		private bool _isWatching;
+
+		public bool IsWatching
+		{
+			get { return _isWatching; }
+			set
+			{
+				_isWatching = value;
+				OnPropertyChanged (new PropertyChangedEventArgs ("IsWatching"));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged (PropertyChangedEventArgs e) 
+		{
+			var handler = PropertyChanged;
+
+			if (handler != null)
+				handler (this, e); 
+		}
+	}
+}
