061dd096d1fefc7beb55f5da65e88625c8ee2d6c
vene <vlad@vene.ro>
2010-09-14 19:41:07 +0300

    Pretty chat with customizable size through app.config

--- ShareTabWin/Helpers/ChatTextBox.cs
+++ ShareTabWin/Helpers/ChatTextBox.cs
@@ -4,6 +4,8 @@ using System.Windows.Documents;
 using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Configuration;
+using System.Collections.Generic;
 namespace ShareTabWin.Helpers
 {
 	/// <summary>
@@ -11,29 +13,33 @@ namespace ShareTabWin.Helpers
 	/// </summary>
 	public partial class ChatTextBox : RichTextBox
 	{
-		// TODO: should be customizable?
-		static int fontsize = 10;
+		private int _fontsize;
 		public ChatTextBox ()
 		{
 			this.IsReadOnly = true;
 			this.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+			if(!Int32.TryParse(ConfigurationManager.AppSettings["fontSize"], out _fontsize)) 
+				// fallback to default font size:
+				_fontsize = 12;
 		}
 
 		public void AddMessage (ChatMessage message)
 		{
 			Paragraph p = new Paragraph();
+			p.Margin = new Thickness (0, 0, 0, 3);
 			
-			var date = new Run (String.Format ("({0})", message.Timestamp.ToShortTimeString ()));
-			date.FontSize = fontsize - 2;
+			var date = new Run (String.Format ("({0}) ", message.Timestamp.ToLongTimeString ()));
+			date.FontSize = _fontsize - 2;
 			date.FontWeight = FontWeights.Bold;
+			date.Foreground = Brushes.DarkGray;
 
-			var username = new Run (message.SenderNickname);
-			username.FontSize = fontsize;
+			var username = new Run (message.SenderNickname + ": ");
+			username.FontSize = _fontsize;
 			username.FontWeight = FontWeights.Bold;
-			username.Foreground = Brushes.DarkTurquoise;
+			username.Foreground = Brushes.DarkOrchid;
 
 			var text = new Run (message.Content);
-			text.FontSize = fontsize;
+			text.FontSize = _fontsize;
 
 			p.Inlines.Add (date);
 			p.Inlines.Add (username);
